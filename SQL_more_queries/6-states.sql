2ea8f1a69dbafc54cb628688ca02000e0066eb30
Jay Cole <jdotcole233@gmail.com>
2023-08-15 06:39:26 -0500

    docs

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- create database to hold states
+
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states 
+(
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
