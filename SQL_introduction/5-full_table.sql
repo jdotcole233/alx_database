c21190439b38e42f7add0cd13bb200e7fe09f142
Jay Cole <jdotcole233@gmail.com>
2023-08-15 06:16:56 -0500

    docs

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,3 @@
 -- full description of table ADD
 SHOW CREATE TABLE first_table;
+
---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- Create a user
+
+CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
+USE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+SET PASSWORD FOR 'user_0d_2'@'localhost'='user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES; 
